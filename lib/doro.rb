1fce244701eeb37c38d1efaafc15fa8d1e4993d6
rpattabi <ragunathan.pattabiraman@gmail.com>
2012-09-30 16:26:42 +0530

    basic refactoring done

--- lib/doro.rb
+++ lib/doro.rb
@@ -5,16 +5,29 @@ class Doro
   include Swt::Widgets
 
   def initialize
+    ui_start
+    setup_tray
+    ui_end
+  end
+
+  def ui_start
     display = Display.get_current
     @shell = Shell.new
     @shell.text = self.class.to_s
+  end
 
+  def ui_end
     @shell.pack
     @shell.open
   end
 
+  def setup_tray
+
+  end
+
   def start
-    display = Swt::Widgets::Display.get_current
+    display = Display.get_current
+
     while !@shell.isDisposed
       display.sleep unless display.read_and_dispatch
     end
